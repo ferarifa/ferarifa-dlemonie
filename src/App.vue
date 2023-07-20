db5c6933ce73180ab03e0435856d81e636997e23
Fera Rifatulmahmudah <139741635+ferarifa@users.noreply.github.com>
2023-07-20 13:42:48 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <Nav />
   <Beranda />
+  <TentangKami />
+  <Rincian />
+  <Produk />
+  <Portofolio />
   <ContactForm />
   <Footer />
 </template>
@@ -8,10 +12,10 @@
 <script setup>
 import Nav from "./components/Beranda/Nav.vue";
 import Beranda from "./components/Beranda/Beranda.vue";
+import TentangKami from "./components/Beranda/TentangKami.vue";
+import Rincian from "./components/Beranda/Rincian.vue";
+import Produk from "./components/Beranda/Produk.vue";
+import Portofolio from "./components/Beranda/Portofolio.vue";
 import ContactForm from "./components/Beranda/ContactForm.vue";
 import Footer from "./components/Beranda/Footer.vue";
 </script>
-
-<style>
-
-</style>

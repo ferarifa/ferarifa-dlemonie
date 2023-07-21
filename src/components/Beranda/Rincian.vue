57bb1a35d4a328f99cc3717d85e3bdb2f87442e2
Fera Rifatulmahmudah <139741635+ferarifa@users.noreply.github.com>
2023-07-21 11:17:06 +0700

    Update Rincian.vue

--- src/components/Beranda/Rincian.vue
+++ src/components/Beranda/Rincian.vue
@@ -1,5 +1,5 @@
 <template>
-      <section id="rincian" class="why-us about">
+      <section id="rincian" class="section-bg about">
     <div class="container-fluid" data-aos="fade-up">
       <div class="section-title">
         <h2>Rincian</h2>

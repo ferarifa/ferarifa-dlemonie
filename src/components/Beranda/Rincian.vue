e09cbf60ca27737bb9a7bc84527bc27e5ff3d736
Fera Rifatulmahmudah <139741635+ferarifa@users.noreply.github.com>
2023-07-21 07:53:32 +0700

    Update Rincian.vue

--- src/components/Beranda/Rincian.vue
+++ src/components/Beranda/Rincian.vue
@@ -15,17 +15,17 @@
             <!-- Button trigger modal -->
       <a href="" class="btn-get-manfaat" data-bs-toggle="modal" data-bs-target="#manfaatdlemonie">Cek Manfaatnya Disini</a>
 
-      <!-- Modal -->
-      <div class="modal fade " id="manfaatdlemonie" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalScrollable">Manfaat D'Lemonie</h5>
-            <button type="button" class="btn close" data-bs-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
+<!-- Modal -->
+<div class="modal fade" id="manfaatdlemonie" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-scrollable">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title">Manfaat D'Lemonie</h5>
+      <button type="button" class="btn close" data-bs-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>      
+    <div class="container modal-body">
             <ul>
         <li><i class="bi-check-square-fill"></i> Menjaga daya tahan tubuh</li>
         <li><i class="bi-check-square-fill"></i> Detoksifikasi toxin dalam tubuh</li>
@@ -54,32 +54,6 @@
     </div>
   </div>
 </div>
-</div>
-
-                <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
 </div>
                 
           </div>
